879d47f4ac370f5a8602d0cf4cdc5d14bd8c0ecc
HaramSs <haram8274@gmail.com>
2024-11-15 10:37:18 +0000

    사용자 컨트롤러 생성#2

---
+++ src/main/java/com/tripcok/tripcokserver/domain/member/controller/MemberController.java
@@ -0,0 +1,10 @@
+package com.tripcok.tripcokserver.domain.member.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vi/member")
+public class MemberController {
+    
+}
