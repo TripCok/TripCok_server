f59efc365653e2c089661e10e009be05ad672437
Jeonghoon2 <ukidd12@naver.com>
2024-11-19 03:57:48 +0900

    [FIX] 여행 관련 Controller Swagger 적용#4

--- src/main/java/com/tripcok/tripcokserver/domain/place/controller/PlaceCategoryController.java
+++ src/main/java/com/tripcok/tripcokserver/domain/place/controller/PlaceCategoryController.java
@@ -2,6 +2,9 @@ package com.tripcok.tripcokserver.domain.place.controller;
 
 import com.tripcok.tripcokserver.domain.place.dto.PlaceCategoryRequest;
 import com.tripcok.tripcokserver.domain.place.service.PlaceCategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,7 @@ import java.nio.file.AccessDeniedException;
 
 @RestController
 @RequestMapping("/api/v1/place/category")
+@Tag(name = "PlaceCategory API", description = "여행 카테고리 API")
 @RequiredArgsConstructor
 public class PlaceCategoryController {
 
@@ -20,12 +24,16 @@ public class PlaceCategoryController {
 
     /* 여행지 카테고리 생성 */
     @PostMapping
+    @Operation(summary = "여행지 카테고리 생성", description = "새로운 카테고리를 생성합니다.")
+    @ApiResponse(responseCode = "201", description = "카테고리가 생성 되었습니다.")
     public ResponseEntity<?> createPlaceCategory(PlaceCategoryRequest request) throws AccessDeniedException {
         return pcs.createCategory(request);
     }
 
     /* 여행지 카테고리 모두 조회 */
     @GetMapping("/all")
+    @Operation(summary = "여행지 카테고리 모두 조회", description = "모든 카테고리를 조회 합니다.")
+    @ApiResponse(responseCode = "200", description = "모든 카테고리가 조회 되었습니다.")
     public ResponseEntity<?> getPlaceCategory() {
         return pcs.findByAllCategory();
     }
--- src/main/java/com/tripcok/tripcokserver/domain/place/controller/PlaceController.java
+++ src/main/java/com/tripcok/tripcokserver/domain/place/controller/PlaceController.java
@@ -3,6 +3,9 @@ package com.tripcok.tripcokserver.domain.place.controller;
 import com.tripcok.tripcokserver.domain.place.dto.PlaceRequest;
 import com.tripcok.tripcokserver.domain.place.dto.PlaceResponse;
 import com.tripcok.tripcokserver.domain.place.service.PlaceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,24 +18,31 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/place")
 @RequiredArgsConstructor
+@Tag(name = "Place API", description = "여행지 API")
 public class PlaceController {
 
     private final PlaceService placeService;
 
     /* 여행지 생성 */
     @PostMapping
+    @Operation(summary = "여행지 생성", description = "새로운 여행지를 등록합니다.")
+    @ApiResponse(responseCode = "201", description = "여행지가 생성 되었습니다.")
     public ResponseEntity<?> createPlace(@RequestBody PlaceRequest.save request) throws AccessDeniedException {
         return placeService.savePlace(request);
     }
 
     /* 여행지 상세 조회 */
     @GetMapping("/{placeId}")
+    @Operation(summary = "여행지 상세 조회", description = "여행지를 상세 조회 합니다.")
+    @ApiResponse(responseCode = "200", description = "여행지를 상세 조회 했습니다.")
     public ResponseEntity<?> getPlaceDetails(@PathVariable Long placeId) {
         return placeService.getPlaceDetails(placeId);
     }
 
     /* 모든 여행지 조회 - 카테고리 별로 필터 가능하게 */
     @GetMapping
+    @Operation(summary = "모든 여행지 조회", description = "선택된 카테고리 별로 모든 여행지를 조회 합니다.")
+    @ApiResponse(responseCode = "200", description = "카테고리 별로 필터된 여행지가 조회 되었습니다.")
     public ResponseEntity<?> getAllPlaces(
             @RequestParam(required = false) List<Long> categoryId,
             Pageable pageable) {
@@ -42,6 +52,8 @@ public class PlaceController {
 
     /* 여행지 삭제*/
     @DeleteMapping("/{placeId}")
+    @Operation(summary = "여행지 삭제", description = "여행지를 삭제합니다.")
+    @ApiResponse(responseCode = "200", description = "해당 여행지를 삭제합니다.")
     public ResponseEntity<?> deletePlace(@PathVariable Long placeId, @RequestParam Long memberId) throws AccessDeniedException {
         return placeService.deletePlace(placeId, memberId);
     }
