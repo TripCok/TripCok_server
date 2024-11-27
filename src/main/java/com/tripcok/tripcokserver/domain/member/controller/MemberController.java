e9a23334c90c4dd91110e5ba54af99d67ea6faec
Jeonghoon2 <ukidd12@naver.com>
2024-11-27 10:26:40 +0900

    [CHORE] 이메일 기능 누락된거 추가

--- src/main/java/com/tripcok/tripcokserver/domain/member/controller/MemberController.java
+++ src/main/java/com/tripcok/tripcokserver/domain/member/controller/MemberController.java
@@ -3,22 +3,45 @@ package com.tripcok.tripcokserver.domain.member.controller;
 import com.tripcok.tripcokserver.domain.member.dto.MemberRequestDto;
 import com.tripcok.tripcokserver.domain.member.dto.MemberResponseDto;
 import com.tripcok.tripcokserver.domain.member.service.MemberService;
+import com.tripcok.tripcokserver.global.service.EmailService;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
+
 @RestController
 @RequestMapping("/api/v1/member")
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
+    private final EmailService emailService;
 
     /* 회원가입 */
     @PostMapping("/register")
     public ResponseEntity<?> createMember(@RequestBody MemberRequestDto.save request) {
         return memberService.createMember(request);
+    }
+
+    /* 회원가입 - 이매일 인증번호 전송 */
+    @GetMapping("/register/{email}")
+    public ResponseEntity<?> sendVerificationEmail(@PathVariable("email") String email) throws MessagingException, UnsupportedEncodingException, MessagingException, UnsupportedEncodingException {
+        log.info(email);
+        return emailService.sendVerificationEmail(email);
+    }
+
+    /* 회원가입 - 이매일 인증번호 인증 */
+    @GetMapping("/register/email/check")
+    public ResponseEntity<?> checkVerificationEmail(
+            @RequestParam("email") String email,
+            @RequestParam("code") String code
+    ) {
+        return emailService.verifyCode(email, code);
     }
 
     /* 로그인 */
