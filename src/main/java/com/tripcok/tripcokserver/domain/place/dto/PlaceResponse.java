e260f9a992c9c0d34e208db7e804a60d0ae4c5cc
mangG907 <mang.gee97@gmail.com>
2024-12-02 15:13:39 +0900

    [CHORE]관리자 페이지 place 생성 및 수정일 에러 해결완료

--- src/main/java/com/tripcok/tripcokserver/domain/place/dto/PlaceResponse.java
+++ src/main/java/com/tripcok/tripcokserver/domain/place/dto/PlaceResponse.java
@@ -5,6 +5,7 @@ import com.tripcok.tripcokserver.domain.place.entity.PlaceCategoryMapping;
 import com.tripcok.tripcokserver.domain.place.entity.PlaceImage;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
@@ -16,6 +17,8 @@ public class PlaceResponse {
     private String address;
     private LocalTime startTime;
     private LocalTime endTime;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
     private List<String> categories;  // 카테고리 이름 리스트
     private List<PlaceImageResponse> images; // 이미지 리스트
 
@@ -26,6 +29,8 @@ public class PlaceResponse {
         this.address = place.getAddress();
         this.startTime = place.getStartTime();
         this.endTime = place.getEndTime();
+        this.createTime = place.getCreateTime();
+        this.updateTime = place.getUpdateTime();
         this.categories = mappings.stream()
                 .map(mapping -> mapping.getCategory().getName())
                 .toList(); // 카테고리 이름 변환
