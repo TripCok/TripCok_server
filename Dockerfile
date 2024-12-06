65320996276e0cba65b792e993474b4c3e1b217b
Jeonghoon2 <ukidd12@naver.com>
2024-12-06 11:19:59 +0900

    [FIX] start-shell fix

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ WORKDIR /app
 
 # JAR 파일 및 설정 파일 복사
 COPY ./build/libs/tripcok-server-0.0.1-SNAPSHOT.jar app.jar
+COPY /spring/env/.env ./
 
 # 컨테이너 실행 시 사용할 사용자 설정
 RUN groupadd -r tripcok && useradd -r -g tripcok tripcok
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SERVER_NAME='tripcok'
-DOCKER_COMPOSE_FILE='/spring/yml/docker-compose.spring.yml'
+DOCKER_COMPOSE_FILE='docker-compose.spring.yml'
 
 # 기존 서버 및 데이터베이스 컨테이너 종료 및 제거
 echo "기존 서버 및 데이터베이스 컨테이너를 종료하고 제거합니다."
