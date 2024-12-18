487186812c22814ae138a461b3fa5710a87827f8
Jeonghoon2 <ukidd12@naver.com>
2024-12-18 16:39:34 +0900

    [FIX] 선호 카테고리 Request Type 변경

--- src/main/java/com/tripcok/tripcokserver/domain/member/controller/MemberController.java
+++ src/main/java/com/tripcok/tripcokserver/domain/member/controller/MemberController.java
@@ -67,7 +67,7 @@ public class MemberController {
     /* 회원 정보 수정 - 선호 카테고리 선택 */
     @PutMapping("/prefer/category")
     public ResponseEntity<?> savePreferCategory(
-            @RequestParam List<Long> categoryIds, HttpSession session) {
+            @RequestBody List<Long> categoryIds, HttpSession session) {
         return memberService.setPreferCategory(categoryIds, session);
     }
 
