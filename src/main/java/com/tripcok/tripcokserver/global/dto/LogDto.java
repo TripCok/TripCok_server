364b5f982e1c0b16f41d869065827ed57ea6597b
lsiwh37249 <lsiwh37249@gmail.com>
2024-12-24 12:17:38 +0900

    [ADD] RequestParam 추가

--- src/main/java/com/tripcok/tripcokserver/global/dto/LogDto.java
+++ src/main/java/com/tripcok/tripcokserver/global/dto/LogDto.java
@@ -1,6 +1,7 @@
 package com.tripcok.tripcokserver.global.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import org.springframework.web.util.ContentCachingRequestWrapper;
@@ -10,6 +11,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Map;
 
 
 @Data
@@ -20,6 +22,7 @@ public class LogDto {
     String clientIp;
     String url;
     String method;
+    String requestParam;
     String request;
     String response;
     String statusCode;
@@ -33,6 +36,7 @@ public class LogDto {
         this.traceId = responseWrapper.getHeader("TRACE_ID");
         this.url = requestWrapper.getRequestURL().toString();
         this.method = requestWrapper.getMethod();
+        this.requestParam = extractRequestParams(requestWrapper); // 요청 파라미터 설정
         this.request = new String(requestWrapper.getContentAsByteArray(), StandardCharsets.UTF_8);
         this.response = new String(responseWrapper.getContentAsByteArray(), StandardCharsets.UTF_8);
         this.statusCode = String.valueOf(responseWrapper.getStatus());
@@ -40,6 +44,14 @@ public class LogDto {
         this.requestTime = LocalDateTime.now();
         this.time = System.currentTimeMillis();
         this.memberId = (String) requestWrapper.getAttribute("memberId");
+    }
+    private String extractRequestParams(ContentCachingRequestWrapper requestWrapper) {
+        try {
+            Map<String, String[]> parameterMap = requestWrapper.getParameterMap();
+            return objectMapper.writeValueAsString(parameterMap); // JSON 형식으로 변환
+        } catch (JsonProcessingException e) {
+            return "{}"; // 변환 실패 시 빈 JSON 반환
+        }
     }
 
 }
